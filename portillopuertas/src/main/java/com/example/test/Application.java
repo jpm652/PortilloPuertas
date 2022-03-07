04496d2d604180f2d84ffe854094234275b89e05
Jose Luis Portillo Martin <jpm652@inlumine.ual.es>
2022-03-07 19:35:10 +0100

    Modification Main

--- portillopuertas/src/main/java/com/example/test/Application.java
+++ portillopuertas/src/main/java/com/example/test/Application.java
@@ -12,6 +12,7 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
     }
 
 }
